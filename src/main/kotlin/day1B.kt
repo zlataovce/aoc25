424fc6e88b57f4a69563e3ec45ec342ff4ac3a93
Matouš Kučera <mk@kcra.me>
2025-12-01 18:26:15 +0100

    shorten a little bit

--- src/main/kotlin/day1B.kt
+++ src/main/kotlin/day1B.kt
@@ -1,10 +1,10 @@
 fun main() = println(
     generateSequence(::readLine)
         .map { it.first() to it.drop(1).toInt() }
-        .fold(50 to 0) { (pos, zer), (dir, step) ->
-            (1..step).fold(pos to zer) { (p, z), _ ->
-                ((p.dec().takeIf { it >= 0 } ?: 99).takeIf { dir == 'L' } ?: p.inc().takeIf { it < 100 } ?: 0)
-                    .run { this to (z.inc().takeIf { this == 0 } ?: z) }
+        .fold(50 to 0) { pz, (dir, step) ->
+            (1..step).fold(pz) { (pos, zer), _ ->
+                ((pos.dec().takeIf { it >= 0 } ?: 99).takeIf { dir == 'L' } ?: pos.inc().takeIf { it < 100 } ?: 0)
+                    .run { this to (zer.inc().takeIf { this == 0 } ?: zer) }
             }
         }
         .second
