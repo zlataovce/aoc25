386b7840a6d6af9d5534542275f3a1ad9bd0f22b
Matouš Kučera <mk@kcra.me>
2025-12-02 19:42:10 +0100

    advent of regex boys

---
+++ src/main/kotlin/day2A.kt
@@ -0,0 +1,8 @@
+fun main() = println(
+    generateSequence(::readLine)
+        .flatMap { it.split(",") }
+        .map { it.split("-") }
+        .flatMap { (f, l) -> f.toLong()..l.toLong() }
+        .filter { Regex("^(.+)\\1$").matches(it.toString()) }
+        .sum()
+)
